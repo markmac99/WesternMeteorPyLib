d29b992607df46eea869154c2592805914721974
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2024-12-15 00:22:38 +0000

    merging upstream changes

--- wmpl/Utils/TrajConversions.py
+++ wmpl/Utils/TrajConversions.py
@@ -221,7 +221,7 @@ def datetime2JD(dt):
 
 
 
-def jd2Date(jd, UT_corr=0, dt_obj=False):
+def jd2Date(jd, UT_corr=0, dt_obj=False, tzinfo=None):
     """ Converts the given Julian date to (year, month, day, hour, minute, second, millisecond) tuple. 
 
     Arguments:
@@ -230,6 +230,7 @@ def jd2Date(jd, UT_corr=0, dt_obj=False):
     Keyword arguments:
         UT_corr: [float] UT correction in hours (difference from local time to UT)
         dt_obj: [bool] returns a datetime object if True. False by default.
+        tzinfo: [timezone object] timezone information for the datetime object
 
     Return:
         (year, month, day, hour, minute, second, millisecond)
@@ -246,10 +247,18 @@ def jd2Date(jd, UT_corr=0, dt_obj=False):
     # library. Time handling should be switched to astropy.time
     except OverflowError:
         date = datetime(MINYEAR, 1, 1, 0, 0, 0)
+    # on rare occasions the date might be not-a-number, we need to trap that. 
+    except ValueError:
+        date = datetime(MINYEAR, 1, 1, 0, 0, 0)
 
 
     # Return a datetime object if dt_obj == True
     if dt_obj:
+
+        # Set the timezone
+        if tzinfo is not None:
+            date = date.replace(tzinfo=tzinfo)
+
         return date
 
     return date.year, date.month, date.day, date.hour, date.minute, date.second, date.microsecond/1000.0
